5b50dccfe00caa9d0a59a656c377c3059494fa69
Fratask <amir.akhatov99@gmail.com>
2019-06-22 14:14:35 +0300

    Added actions Register, Login, DeleteRegistration

---
+++ src/test/java/ru/fratask/mc/plugin/auth/test/action/Login.java
@@ -0,0 +1,10 @@
+package ru.fratask.mc.plugin.auth.test.action;
+
+import ru.fratask.mc.plugin.auth.test.entity.GamePlayer;
+
+public class Login {
+
+    public static void login(GamePlayer gamePlayer){
+
+    }
+}
---
+++ src/test/java/ru/fratask/mc/plugin/auth/test/action/Register.java
@@ -0,0 +1,10 @@
+package ru.fratask.mc.plugin.auth.test.action;
+
+import ru.fratask.mc.plugin.auth.test.entity.GamePlayer;
+
+public class Register {
+
+    public static void register(GamePlayer gamePlayer){
+
+    }
+}
